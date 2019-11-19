da16f2e10397c2683a01419df608e5bee18ea803
taozi <449141360@qq.com>
2019-11-19 09:53:27 +0800

    二维数组中的查找

--- 1.cpp
+++ 1.cpp
@@ -3,8 +3,31 @@
 #include <algorithm>
 using namespace std;
 
+//二维数组中的查找
 bool Find(int target, vector<vector<int>> array)
 {
+    if (array.size() == 0 || array[0].size() == 0)
+    {
+        return false;
+    }
+    int m = array.size(), n = array[0].size();
+    int i = m - 1, j = 0;
+    while (i >= 0 && j < n)
+    {
+        if (target < array[i][j])
+        {
+            i--;
+        }
+        else if (target > array[i][j])
+        {
+            j++;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    return false;
 }
 
 void test()
