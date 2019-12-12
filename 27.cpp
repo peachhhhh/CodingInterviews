9a2d260af9c024e750fdfe76728221bad81677d5
taozi <449141360@qq.com>
2019-12-12 10:55:24 +0800

    字符串的排列

---
+++ 27.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void dfs(vector<string> &res, string s, int pos)
+{
+    if (pos == s.size() - 1)
+    {
+        if (find(res.begin(), res.end(), s) == res.end())
+        {
+            res.push_back(s); //如果res中已有重复结果，则不push
+        }
+        return;
+    }
+    for (int i = pos; i < s.size(); i++)
+    {
+        swap(s[i], s[pos]);
+        dfs(res, s, pos + 1);
+        swap(s[i], s[pos]);
+    }
+}
+
+//字符串的排列
+vector<string> Permutation(string str)
+{
+    vector<string> res;
+    dfs(res, str, 0);
+    sort(res.begin(), res.end()); //转换成字典序
+    return res;
+}
+
+void test()
+{
+}
+
+int main()
+{
+    test();
+    return 0;
+}
